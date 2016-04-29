c616424584632bed3c6b4492c4b43fec4bade70d
nikthequik <quik.the.nick@gmail.com>
2016-04-29 10:22:34 -0600

    -create state machine for ryu -include input from braden

--- js/app.js
+++ js/app.js
@@ -1,41 +1,85 @@
 $(function() {
-$('.ryu').on('mousedown', function() {
-	playHadouken();
-	$('.ryu-ready').hide();
-	$('.ryu-still').hide();
-	$('.ryu-throwing').show();
-	$('.hadouken').finish().show().animate({
-		left: '1020px'
-		}, 800, function() {
-			$(this).hide();
-			$(this).css({
-			left: '520px'
-		});
-	})
+
+	var ryuState = "ryu-still";
+	var ryuTakingAction = "";
+	var ryuHide = function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
+		$('.ryu-ready').hide();
+		$('.nice').hide();
+	};
+
+	$('.ryu').on('mousedown', function() {
+		if (!ryuTakingAction) {
+			ryuTakingAction = "ryu-throwing";
+			ryuState = "ryu-throwing";
+			playHadouken();
+			ryuHide();
+			$('.ryu-throwing').show();
+			$('.nice').show();
+			$('.hadouken').finish().show().animate({
+				left: '1220px'
+				}, 800, function() {
+					$(this).hide();
+					$(this).css({
+					left: '520px'
+				});
+			});
+		}
+	});
+
+	$('.ryu').on('mouseup', function() {
+		ryuHide();
+		$('.ryu-ready').show();
+		ryuTakingAction = "";
+	});
+
+	$('.ryu').on('mouseenter', function() {
+		ryuState = "ryu-ready";
+		ryuHide();
+		$('.ryu-ready').show();
+	});
+
+	$('.ryu').on('mouseleave', function() {
+		ryuState = "ryu-still";
+		ryuHide();
+		$('.ryu-still').show();
+	});
+
+	$(window).on('keydown', (function(e) {
+		console.log(e);
+		if (e.keyCode === 88){
+			if (!ryuTakingAction) { 
+				ryuTakingAction = "ryu-cool";
+				ryuHide();
+				$('.ryu-cool').show();
+			}
+		};
+	}));
+
+	$(window).on('keyup', (function(e) {
 	
-});
+		if (e.keyCode === 88){
+			ryuTakingAction = "";
+			ryuHide();
 
-$('.ryu').on('mouseup', function() {
-	$('.ryu-throwing').hide();
-	$('.ryu-still').hide();
-	$('.ryu-ready').show();
-});
+			if (ryuState === "ryu-still") {
+				$('.ryu-still').show();
+			}
+			else if (ryuState === "ryu-throwing") {
+				$('.ryu-throwing').show();
+			}
 
-$('.ryu').on('mouseenter', function() {
-	$('.ryu-still').hide();
-	$('.ryu-throwing').hide();
-	$('.ryu-ready').show();
-});
+			else{
+				$('.ryu-ready').show();
+			}
+		};
+	}));
 
-$('.ryu').on('mouseleave', function() {
-	$('.ryu-ready').hide();
-	$('.ryu-throwing').hide();
-	$('.ryu-still').show();
-});
-
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
+	function playHadouken () {
+	  $('#hadouken-sound')[0].volume = 0.5;
+	  $('#hadouken-sound')[0].load();
+	  $('#hadouken-sound')[0].play();
+	}
 });
